6c494d9827420a6784fd4e4b8581bd8956e4a8db
Andy Million <amillion3@gmail.com>
2019-01-01 14:01:53 -0600

    Added models

---
+++ nss/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nss.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string NameFirst { get; set; }
+        public string NameLast { get; set; }
+        public string Slack { get; set; }
+    }
+}
